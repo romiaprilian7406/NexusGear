f5aa2e010b7d5f15a2f2d4a7f39782d72fe19b52
Romi Aprilian Mustafa <148515374+romiaprilian7406@users.noreply.github.com>
2026-05-11 07:25:12 +0700

    Update sql files

---
+++ update_product_images.sql
@@ -0,0 +1,32 @@
+-- Update product image URLs with brand-specific placeholder images
+-- Run this in Supabase SQL Editor
+
+UPDATE products SET image_url = 'https://assets2.razerzone.com/images/pnx.assets/b2365984c607948cd60ebe738ea2e6b8/razer-blackwidow-v4-hero-desktop.webp' WHERE name = 'Razer BlackWidow V4';
+UPDATE products SET image_url = 'https://resource.logitechg.com/w_544,h_466,ar_7:6,c_pad,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/gaming/en/products/pro-x-tkl/gallery-2-pro-x-tkl-black-lightspeed-gaming-keyboard.png.png' WHERE name = 'Logitech G Pro X';
+UPDATE products SET image_url = 'https://assets.corsair.com/image/upload/c_pad,q_85,h_1100,w_1100,f_auto/products/Gaming-Keyboards/CH-9109012-NA/Gallery/K70_RGB_MK2_02.webp' WHERE name = 'Corsair K70 RGB MK.2';
+UPDATE products SET image_url = 'https://images.ctfassets.net/hmm5mo4qf4mf/2WOla4vt9jRRLTos2e9QwH/87d1b1bc9a5b1c5f35be44a80adfe2ec/apex_pro_img_buy_04.png__1920x1080_crop-fit_optimize_subsampling-2-2139.png?fm=webp&q=90&fit=scale&w=1920' WHERE name = 'SteelSeries Apex Pro';
+
+UPDATE products SET image_url = 'https://assets2.razerzone.com/images/pnx.assets/ef0800b3d46c633e4fac8bedf33fc991/dav3hyperspeed-desktop-hero.webp' WHERE name = 'Razer DeathAdder V3';
+UPDATE products SET image_url = 'https://resource.logitechg.com/w_544,h_466,ar_7:6,c_pad,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/gaming/en/products/g502x-plus/gallery/g502x-plus-gallery-2-black.png' WHERE name = 'Logitech G502 X Plus';
+UPDATE products SET image_url = 'https://images.ctfassets.net/hmm5mo4qf4mf/40YXOfd920tkMqbv4fdUin/ef3f925af8f9144d848c4392d25c0b3a/05_rival600_kv_top_hero.png__1920x1080_crop-fit_optimize_subsampling-2-859.png?fm=webp&q=90&fit=scale&w=1920' WHERE name = 'SteelSeries Rival 600';
+UPDATE products SET image_url = 'https://dlcdnwebimgs.asus.com/gain/07CE8835-80CB-4A20-818C-9FFDAB0367CA/w717/h525/fwebp' WHERE name = 'ASUS ROG Keris Wireless';
+
+UPDATE products SET image_url = 'https://hyperx.com/cdn/shop/files/hyperx_cloud_alpha_blackred_1_main.jpg?v=1763563198' WHERE name = 'HyperX Cloud Alpha';
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/71CT4h6bDSL._AC_SL1500_.jpg' WHERE name = 'Razer BlackShark V2';
+UPDATE products SET image_url = 'https://d1q3zw97enxzq2.cloudfront.net/images/HS80_MAX_WIRELESS_ANTHRACITE_REN.width-1000.format-webp_iDqn3MH.webp' WHERE name = 'Corsair HS80 RGB Wireless';
+
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/51qJMrJKlEL._AC_SL1000_.jpg' WHERE name = 'ASUS ROG Swift 27" 144Hz';
+UPDATE products SET image_url = 'https://storage-asset.msi.com/global/picture/product/product_1609380485465e07f5a2b6d0df06835ad024a32e73.webp' WHERE name = 'MSI Optix MAG274QRF';
+UPDATE products SET image_url = 'https://media.us.lg.com/transform/ecomm-PDPGallery-1100x730/0d1e62da-95ec-40e6-b2a3-0ef94b03aea3/md08000370-Z-01-jpg?io=transform:fill,width:1536' WHERE name = 'LG UltraGear 27GP850';
+
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/31AROTdFEzL._AC_SL1050_.jpg' WHERE name = 'SteelSeries QcK XXL';
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/81ACRfPHn-L._AC_SL1500_.jpg' WHERE name = 'Razer Goliathus Extended';
+UPDATE products SET image_url = 'https://assets.corsair.com/image/upload/c_pad,q_85,h_1100,w_1100,f_auto/products/Gaming-Mousepads/base-mm350-pro-config/Gallery/MM350_PRO_Extended_XL_BLACK_01.webp' WHERE name = 'Corsair MM350 Pro XL';
+
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/51PmeLGEkML._SL1500_.jpg' WHERE name = 'DualSense PS5 Controller';
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/51ZUlhG3n-L._SL1000_.jpg' WHERE name = 'Xbox Wireless Controller';
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/61wnxkktl5L._SL1500_.jpg' WHERE name = '8BitDo Pro 2 Controller';
+
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/410uYasNqFL._AC_SL1000_.jpg' WHERE name = 'Secretlab TITAN Evo 2022';
+UPDATE products SET image_url = 'https://m.media-amazon.com/images/I/51uuAT6xypL._AC_SL1500_.jpg' WHERE name = 'DXRacer Formula Series';
+UPDATE products SET image_url = 'https://dlcdnwebimgs.asus.com/gain/5BDB7256-2144-491E-AD13-95FB9179BE68/w717/h525/fwebp' WHERE name = 'ASUS ROG Chariot Core';
